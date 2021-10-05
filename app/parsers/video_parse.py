06c59da70f0fa65007f664b3562f669dd5cd23be
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-10-06 00:39:43 +0300

    small fixes

--- app/parsers/video_parse.py
+++ app/parsers/video_parse.py
@@ -3,17 +3,16 @@ from youtubesearchpython import CustomSearch, VideoSortOrder
 from googleapiclient.discovery import build
 
 
-
 class YoutubeSearcher:
 
-    def __init__(self, video_name: str, limit: int, comments_number: int):
+    def __init__(self, video_name: str, videos_number: int, comments_number: int):
         self.video_name = video_name
-        self.limit = limit
+        self.videos_number = videos_number
         self.comments_number = comments_number
-        self.video_by_views = CustomSearch(self.video_name, VideoSortOrder.viewCount, limit=limit)
+        self.video_by_views = CustomSearch(self.video_name, VideoSortOrder.viewCount, limit=videos_number)
         self.__api = 'AIzaSyAPo1tx5P0Q8NqsJSLz1APFAvHnCT9UWtQ'
 
-    def filter_results(self) -> list:
+    def filter_results(self) -> List:
         videos_by_vews = self.video_by_views.result()
         info_list = []
         for info in videos_by_vews['result']:
@@ -29,17 +28,14 @@ class YoutubeSearcher:
             
         return info_list
     
-    def video_comments(self, video_id: int, comments_number: int) -> list:
-        youtube = build('youtube', 'v3',
-                        developerKey=self.__api)
-
+    def video_comments(self, video_id: int, comments_number: int) -> List:
+        youtube = build('youtube', 'v3', developerKey=self.__api)
         request = youtube.commentThreads().list(
             part='snippet',
             videoId=video_id,
             maxResults=comments_number,
         )
         response = request.execute()
-
         comments_list = []
         for item in response['items']:
             comment = item['snippet']['topLevelComment']['snippet']['textOriginal']
@@ -47,4 +43,6 @@ class YoutubeSearcher:
 
         return comments_list
 
-# searcher = YoutubeSearcher('Тестировщик', 4, 3).filter_results()
+# searcher = YoutubeSearcher(video_name='Тестировщик', videos_number=5, comments_number=5).filter_results()
+# with open('data.json', 'w', encoding='utf-8') as f:
+#     json.dump(searcher, f, ensure_ascii=False, indent=4)
