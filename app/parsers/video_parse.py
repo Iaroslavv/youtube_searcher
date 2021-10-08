81df7da3d0d3f6225aea73f9c88f4cc9dae7a74c
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-10-08 22:04:10 +0300

    add parsers by search term

--- app/parsers/video_parse.py
+++ app/parsers/video_parse.py
@@ -1,21 +1,37 @@
+from search_config import SearchItems
+
 from typing import List
+
 from youtubesearchpython import CustomSearch, VideoSortOrder
 from googleapiclient.discovery import build
 
 
 class YoutubeSearcher:
 
-    def __init__(self, video_name: str, videos_number: int, comments_number: int):
+    def __init__(self, video_name: str, videos_number: int, comments_number: int, search_by=None):
         self.video_name = video_name
         self.videos_number = videos_number
         self.comments_number = comments_number
+        self.search_by = search_by
         self.video_by_views = CustomSearch(self.video_name, VideoSortOrder.viewCount, limit=videos_number)
+        self.video_by_rating = CustomSearch(self.video_name, VideoSortOrder.rating, limit=videos_number)
+        self.video_by_upload_date = CustomSearch(self.video_name, VideoSortOrder.uploadDate, limit=videos_number)
         self.__api = 'AIzaSyAPo1tx5P0Q8NqsJSLz1APFAvHnCT9UWtQ'
 
     def filter_results(self) -> List:
-        videos_by_vews = self.video_by_views.result()
+        if self.search_by == SearchItems.views:
+            videos_by_vews = self.video_by_views.result()
+            return self.parse_result(videos_by_vews)
+        elif self.search_by == SearchItems.rating:
+            videos_by_rating = self.video_by_rating.result()
+            return self.parse_result(videos_by_rating)
+        elif self.search_by == SearchItems.upload_time:
+            videos_by_upload_time = self.video_by_upload_date.result() 
+            return self.parse_result(videos_by_upload_time)       
+    
+    def parse_result(self, by_term):
         info_list = []
-        for info in videos_by_vews['result']:
+        for info in by_term['result']:
             comments = self.video_comments(info['id'], self.comments_number)
             info_dict = {
                 'title': info['title'],
@@ -43,6 +59,6 @@ class YoutubeSearcher:
 
         return comments_list
 
-# searcher = YoutubeSearcher(video_name='Тестировщик', videos_number=5, comments_number=5).filter_results()
-# with open('data.json', 'w', encoding='utf-8') as f:
+# searcher = YoutubeSearcher(video_name='Тестировщик', videos_number=5, comments_number=5, search_by='Upload Time').filter_results()
+# with open('upload.json', 'w', encoding='utf-8') as f:
 #     json.dump(searcher, f, ensure_ascii=False, indent=4)
