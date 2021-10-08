bdf28549f423e18d16d21f8050ee3b7dcf6e2c9e
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-10-09 00:00:20 +0300

    add exception handling

--- app/parsers/video_parse.py
+++ app/parsers/video_parse.py
@@ -1,9 +1,10 @@
-from search_config import SearchItems
+from app.parsers.search_config import SearchItems
 
-from typing import List
+from typing import Union
 
 from youtubesearchpython import CustomSearch, VideoSortOrder
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class YoutubeSearcher:
@@ -18,7 +19,7 @@ class YoutubeSearcher:
         self.video_by_upload_date = CustomSearch(self.video_name, VideoSortOrder.uploadDate, limit=videos_number)
         self.__api = 'AIzaSyAPo1tx5P0Q8NqsJSLz1APFAvHnCT9UWtQ'
 
-    def filter_results(self) -> List:
+    def filter_results(self) -> list:
         if self.search_by == SearchItems.views:
             videos_by_vews = self.video_by_views.result()
             return self.parse_result(videos_by_vews)
@@ -28,7 +29,7 @@ class YoutubeSearcher:
         elif self.search_by == SearchItems.upload_time:
             videos_by_upload_time = self.video_by_upload_date.result() 
             return self.parse_result(videos_by_upload_time)       
-    
+
     def parse_result(self, by_term):
         info_list = []
         for info in by_term['result']:
@@ -38,27 +39,31 @@ class YoutubeSearcher:
                 'url': f"https://www.youtube.com/watch?v={info['id']}",
                 'duration': info['duration'],
                 'views': info['viewCount']['short'],
-                'comments': comments
+                'comments': 'Comments are disabled' if comments is None else comments
             }
             info_list.append(info_dict)
-            
+
         return info_list
     
-    def video_comments(self, video_id: int, comments_number: int) -> List:
+    def video_comments(self, video_id: int, comments_number: int) -> Union[list, str]:
         youtube = build('youtube', 'v3', developerKey=self.__api)
         request = youtube.commentThreads().list(
             part='snippet',
             videoId=video_id,
             maxResults=comments_number,
         )
-        response = request.execute()
         comments_list = []
-        for item in response['items']:
-            comment = item['snippet']['topLevelComment']['snippet']['textOriginal']
-            comments_list.append(comment)
-
+        try:
+            response = request.execute()
+            for item in response['items']:
+                comment = item['snippet']['topLevelComment']['snippet']['textOriginal']
+                comments_list.append(comment)
+        except HttpError:
+            comments_list = None
         return comments_list
 
+
+# DEBUGGING
 # searcher = YoutubeSearcher(video_name='Тестировщик', videos_number=5, comments_number=5, search_by='Upload Time').filter_results()
 # with open('upload.json', 'w', encoding='utf-8') as f:
 #     json.dump(searcher, f, ensure_ascii=False, indent=4)
